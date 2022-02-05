fc5f6274092e74a7a61be7a340a67cccd1657370
Phil Olinger <philip.olinger@gmail.com>
2022-02-05 14:59:43 -0500

    Updated XboxController calls for 2022

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
@@ -61,10 +62,18 @@ public class Robot extends TimedRobot {
      * CAN Bus device IDS: 
      * https://docs.wpilib.org/en/latest/docs/software/can-devices/can-addressing.html
      */
-    m_leftFrontMotor = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
-    m_rightFrontMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-    m_leftRearMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-    m_rightRearMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+    
+    
+    m_rightFrontMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+    m_rightRearMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    // m_rightMiscMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
+    m_leftRearMotor = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
+    m_leftFrontMotor = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
+    // m_rightMiscMotor = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless)
+
+    m_compressor = new Compressor();
+    m_stick = new XboxController(0);
+
     m_leftMotors = new SpeedControllerGroup(m_leftFrontMotor, m_leftRearMotor);
     m_rightMotors = new SpeedControllerGroup(m_rightFrontMotor, m_rightRearMotor);
     m_robotDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
@@ -84,16 +93,20 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
+    if (m_timer.get() < 3.0) {
       /**
        * At first, we can operate individual motors for 2 seconds here for debugging
        * Later, we will operate the robotDrive subclass to drive the robot
        */
+      // m_leftMotors.set(0.5);
+      // m_rightMotors.set(0.5);
 
+      m_robotDrive.arcadeDrive(-0.5, 0.0); // drive forwards half speed
 
-      // m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
     } else {
       m_robotDrive.stopMotor(); // stop robot
+      // m_leftMotors.stopMotor();
+      // m_rightMotors.stopMotor();
     }
   }
 
@@ -109,7 +122,24 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    // m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    // m_robotDrive.arcadeDrive(-m_stick.getY(Hand.kLeft), m_stick.getX(Hand.kLeft));
+    
+    // For tank drive, we need two sticks
+    
+    double y_val = m_stick.getRightY();
+    double x_val = m_stick.getLeftY();
+    // Keep the sign for later on
+    double y_sign = -1.0;
+    double x_sign = -1.0;
+    if(y_val < 0){
+      y_sign = -y_sign;
+    }
+    if(x_val < 0){
+      x_sign = -x_sign;
+    }
+    // Square the inputs for exponential response, but keep the sign
+    m_robotDrive.tankDrive((y_sign * (y_val * y_val)),
+                           (x_sign * (x_val * x_val)));
   }
 
   /**
